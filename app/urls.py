872c7f9c55cb560013972b04f1a80c973a889a5b
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-30 17:07:30 -0500

    adding media files'

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('search/', views.SearchResultsView, name='search'),
+    path('prac/', views.prac, name='prac'),
+    
+]
