d3809ff860c8ba8764282fa3aa2e1b050f367bdf
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-30 19:06:01 -0500

    fixing main issue

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+
+# # Create your models here.
+# class City(models.Model):
+#     name = models.CharField(max_length=25)
+#     city_image = models.ImageField(upload_to='media', default=None)
+    # we will see how this works 
+
+    # def __str__(self):
+    #     return self.name 
+        
+    # class Meta:
+    #     unique_together = ['name']
+    #     verbose_name_plural = 'cities'
+
+    
